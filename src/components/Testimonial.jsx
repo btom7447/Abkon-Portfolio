56d1f24407bfa00557a2e0669e1b2b24d59bed67
btom7447 <tombenjamin7447@gmail.com>
2024-07-25 17:21:58 +0100

    changes to testimonial component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -21,24 +21,24 @@ const Testimonial = () => {
 
   return (
     <div className="testimonial-section">
-        <div className="testimonial-caption">
-            <h1>Client Testimonials</h1>
-            <p>
-            My clients' success is my greatest achievement. We're proud to have helped numerous brands elevate their visual identity and achieve their goals. But don't just take my word for it - hear from the clients who have experienced the impact of my design expertise firsthand.
-            </p>
-        </div>
+      <div className="testimonial-caption">
+        <h1>Client Testimonials</h1>
+        <p>
+          My clients' success is my greatest achievement. We're proud to have helped numerous brands elevate their visual identity and achieve their goals. But don't just take my word for it - hear from the clients who have experienced the impact of my design expertise firsthand.
+        </p>
+      </div>
         <div className="splide splide-testimonial">
-            <div className="splide__track">
-                <ul className="splide__list">
-                    {testimonialData.map((testimonial) => (
-                        <li key={testimonial.id} className="splide__slide">
-                            <div className="testimonial-slide">
-                                <span className='quotation'>"</span>
-                                <img src={testimonial.image} alt={testimonial.name} />
-                                <p>{testimonial.text}</p>
-                                <h5>{testimonial.name} - <span className='title'>{testimonial.title}</span></h5>
-                            </div>
-                        </li>
+          <div className="splide__track">
+            <ul className="splide__list">
+              {testimonialData.map((testimonial) => (
+                <li key={testimonial.id} className="splide__slide">
+                  <div className="testimonial-slide">
+                    <span className='quotation'>"</span>
+                    <img src={testimonial.image} alt={testimonial.name} />
+                    <p>{testimonial.text}</p>
+                    <h5>{testimonial.name} <br /> <span className='title'>{testimonial.title}</span></h5>
+                  </div>
+                </li>
                     ))}
                 </ul>
             </div>
